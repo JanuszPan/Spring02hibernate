6fe4b979c2dd223485e3dd869391a5c80813cf29
JanuszPan <janusz.panczyk@gmail.com>
2021-04-06 15:59:55 +0200

    data.sql

--- src/main/resources/META-INF/sql/data.sql
+++ src/main/resources/META-INF/sql/data.sql
@@ -1,7 +1,7 @@
 insert into publishers(name,regon,nip) values ('Helion',337473111,5094884892), ('Atena',370163623,5274209258);
 insert into authors(firstName, lastName, pesel,email) values ('Jan', 'Kowalski',73051969411,'jan.kowalski@wp.pl'), ('Anna', 'Nowak',50021177926,'anna.nowak@wp.pl');
 insert into books( description,rating, title, pages,publisher_id) values ('powieść historyczna',5,'Ogniem i mieczem',200,1),('podręcznik',6,'Java',300,2);
-insert into category(name ) values ('literatura piękna'),('literatura naukowa');
+insert into category(name ) values ('literatura piękna'),('literatura naukowa'),('literatura obca');
 
 
 
