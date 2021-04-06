ede17310d6970d05089589d8e8a768fd48e5b103
JanuszPan <janusz.panczyk@gmail.com>
2021-04-06 12:29:42 +0200

    Student-Lombok

--- src/main/java/pl/coderslab/other/Student.java
+++ src/main/java/pl/coderslab/other/Student.java
@@ -1,7 +1,13 @@
 package pl.coderslab.other;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+@Data //Lombok sam generuje settery, gettery, toString i wiele innych
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     String firstName;
     String lastName;
@@ -11,68 +17,4 @@ public class Student {
     boolean mailingList;
     List programmingSkills;
     List hobbies;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public boolean isMailingList() {
-        return mailingList;
-    }
-
-    public void setMailingList(boolean mailingList) {
-        this.mailingList = mailingList;
-    }
-
-    public List getProgrammingSkills() {
-        return programmingSkills;
-    }
-
-    public void setProgrammingSkills(List programmingSkills) {
-        this.programmingSkills = programmingSkills;
-    }
-
-    public List getHobbies() {
-        return hobbies;
-    }
-
-    public void setHobbies(List hobbies) {
-        this.hobbies = hobbies;
-    }
 }
